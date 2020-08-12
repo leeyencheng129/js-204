1d7cafed9562b23eee2e8a622068b2c4d12ee047
Anna <55761750+Pull-Plus@users.noreply.github.com>
2020-08-12 14:11:23 +0800

    UP

---
+++ code/0812/3.todo-呈現/index.js
@@ -0,0 +1,32 @@
+// 讓程式碼簡短一些
+const byId = (id) => document.getElementById(id)
+
+// 先獲得目前的元件參照
+const todoAdd = byId('todo-add')
+const todoInput = byId('todo-input')
+const todoList = byId('todo-list')
+
+// 所有的待辦事項要放在這個陣列中
+const todos = []
+
+// 用於呈現項目到網頁上的函式
+const displayTodoList = () => {
+  let display = ''
+
+  for (let i = 0; i < todos.length; i++) {
+    display += '<li>' + todos[i] + '</li>'
+  }
+
+  todoList.innerHTML = '<ul>' + display + '</ul>'
+}
+
+todoAdd.addEventListener('click', () => {
+  // 加入 todoInput 輸入值到 todos 陣列
+  if (todoInput.value.trim()) todos.unshift(todoInput.value)
+
+  //每輸入一次清除 todoInput
+  todoInput.value = ''
+
+  // 呼叫呈現的函式
+  displayTodoList()
+})
